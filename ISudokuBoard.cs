86ce01ded1c301ee2e0008f93c2a75aa41bbf1cf
Your Name <you@example.com>
2026-02-03 10:39:00 +0200

    Created an Interfaces folder. created the ISudokuBoard<T> generic interface, including Indexer for board access, IsEmpty method, ToSimpleString method, and Size property.

---
+++ ISudokuBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public interface ISudokuBoard<T> where T : IEquatable<T>
+{
+
+    bool IsEmpty(int row, int col);
+
+    T this[int row, int col]
+    {
+        get;
+        set;
+    }
+    int Size { get; }
+    string ToSimpleString();
+}
+
+
