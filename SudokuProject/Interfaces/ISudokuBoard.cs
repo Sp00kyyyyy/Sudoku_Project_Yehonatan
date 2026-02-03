afd99fe6e73e256cac1570168203b79d698d4174
Your Name <you@example.com>
2026-02-03 17:32:04 +0200

    Inside a models folder: created SudokuBoard class with interface compliance, constructor, Tostring and string conversions. also added a namespace for the SudokuBoard Interface

---
+++ SudokuProject/Interfaces/ISudokuBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject.Interfaces
+{
+    public interface ISudokuBoard<T> where T : IEquatable<T>
+    {
+        bool IsEmpty(int row, int col); T this[int row, int col]
+        {
+            get;
+            set;
+        }
+        int Size
+        {
+            get;
+        }
+        string ToSimpleString();
+    }
+}
---
+++ SudokuProject/Models/SudokuBoard.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuProject.Interfaces;
+
+namespace SudokuProject.Models
+{
+    public class SudokuBoard : ISudokuBoard<int>
+    {
+        private int[,] Board;
+        public int Size { get { return this.Board.GetLength(0); } }
+
+        public int this[int row, int col]
+        {
+            get => this.Board[row, col];
+            set => this.Board[row, col] = value;
+        }
+
+        public SudokuBoard(int size)
+        {
+            this.Board = new int[size, size];
+        }
+
+
+        public bool IsEmpty(int row, int col)
+        {
+            if (Board[row, col].Equals(0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string ToSimpleString()
+        {
+            string simpleString = "";
+            for (int i = 0; i < this.Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.Board.GetLength(1); j++)
+                {
+                    simpleString += this.Board[i, j].ToString();
+                }
+            }
+            return simpleString;
+        }
+        public override string ToString()
+        {
+            StringBuilder strBuild = new StringBuilder();
+            int rows = this.Size;
+            int cols = this.Board.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    strBuild.Append($"| {this.Board[i, j]} ");
+                }
+                strBuild.AppendLine("|");
+
+            }
+            return strBuild.ToString();
+        }
+    }
+}
