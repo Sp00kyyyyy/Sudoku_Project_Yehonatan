8a595a1b4916228106b85e81927811a0b9892b7e
Your Name <you@example.com>
2026-02-05 14:14:53 +0200

    Created IParser interface and StringParser class
    Created generic IParser<T> interface with ParseInput function in Interfaces folder
    Created StringParser class in IO folder that implements the IParser logic. Created in order to convert string input into SudokuBoard

---
+++ SudokuProject/IO/StringParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuProject.Interfaces;
+using SudokuProject.Models;
+
+namespace SudokuProject.IO
+{
+    public class StringParser : IParser<int>
+    {
+        public ISudokuBoard<int> ParseInput(string input, int size)
+        {
+            SudokuBoard board = new SudokuBoard(size);
+            int rows = size;
+            int cols = size;
+            int index = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    board[i, j] = int.Parse(input[index].ToString());
+                    index++;
+                }
+            }
+            return board;
+        }
+    }
+}
---
+++ SudokuProject/Interfaces/Iparser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject.Interfaces
+{
+    public interface IParser<T> where T : IEquatable<T>
+    {
+        ISudokuBoard<T> ParseInput(string input, int size);
+    }
+}
