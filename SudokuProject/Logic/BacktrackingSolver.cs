3edcf7499437686850e189379f5d4ffbc0a9bf02
Your Name <you@example.com>
2026-02-06 15:06:52 +0200

    Created the solving infrastructure in the project:
    Interfaces Folder (SudokuProject/Interfaces):
       Added the ISolver interface. This defines the standard contract that any solver in the system must follow.
    
    Logic Folder (SudokuProject/Logic):
    Created the BacktrackingSolver class to implement the solving logic.
    Recursive Algorithm: Created the Solve function that uses backtracking to fill the board and backtrack if a path fails.
    Optimization: created FindBestCell with the MRV Logic. It prioritizes cells with the fewest legal options to solve the board much faster.
    Validation: Added IsValidMove function to check numbers against the game rules.

---
+++ SudokuProject/Logic/BacktrackingSolver.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuProject.Interfaces;
+
+namespace SudokuProject.Logic
+{
+    public class BacktrackingSolver : ISolver<int>
+    {
+        private readonly List<ISudokuRule<int>> LogicRules;
+        public BacktrackingSolver(List<ISudokuRule<int>> logicRules)
+        {
+            this.LogicRules = logicRules;
+        }
+        private bool IsValidMove(int row, int col, ISudokuBoard<int> board, int number)
+        {
+            for (int i = 0; i < this.LogicRules.Count; i++)
+            {
+                if (!(this.LogicRules[i].IsValid(row, col, board, number)))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private (int row, int col) FindBestCell(ISudokuBoard<int> board)
+        {
+            int bestcol = -1;
+            int bestRow = -1;
+            int minOptions = 10;
+
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    int count = 0;
+                    if (board.IsEmpty(i, j))
+                    {
+                        for (int z = 1; z <= 9; z++)
+                        {
+                            if (IsValidMove(i, j, board, z))
+                            {
+                                count++;
+                            }
+                        }
+                        if (count < minOptions)
+                        {
+                            minOptions = count;
+                            bestcol = j;
+                            bestRow = i;
+                        }
+                        if (minOptions == 1)
+                            return (bestRow, bestcol);
+
+                    }
+                }
+            }
+            return (bestRow, bestcol);
+        }
+        public bool Solve(ISudokuBoard<int> board)
+        {
+            var index = FindBestCell(board);
+            int row = index.Item1;
+            int col = index.Item2;
+            if (col == -1 || row == -1)
+            {
+                return true;
+            }
+
+
+            for (int num = 1; num <= 9; num++)
+            {
+                if (IsValidMove(row, col, board, num))
+                {
+                    board[row, col] = num;
+                    if (Solve(board))
+                    {
+                        return true;
+                    }
+                    board[row, col] = 0;
+                }
+            }
+            return false;
+        }
+    }
+}
