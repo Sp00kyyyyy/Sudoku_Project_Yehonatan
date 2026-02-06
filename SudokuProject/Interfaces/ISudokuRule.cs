3d3b2cfc5e99be79140545c2c4185f8f89ff4313
Your Name <you@example.com>
2026-02-06 12:35:43 +0200

    Created the Logic folder for the game logic files and inside it the Rules folder for the game rules
    Created BoxRule.cs to ensure a number appears only once in its 3x3 square
    Created ColumnRule.cs to ensure a number appears only once in its column
    Created RowRule.cs to ensure a number appears only once in its row
    
    Created ISudokuRule.cs in the Interfaces folder
    This interface defines the IsValid function that checks if a move is allowed

---
+++ SudokuProject/Interfaces/ISudokuRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject.Interfaces
+{
+    public interface ISudokuRule<T> where T : IEquatable<T>
+    {
+        bool IsValid(int row, int col, ISudokuBoard<T> board, T number);
+    }
+}
---
+++ SudokuProject/Logic/GameRules/ColumnRule.cs
@@ -0,0 +1,25 @@
+﻿using NPOI.SS.Formula.Functions;
+using SudokuProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject.Logic.GameRules
+{
+    public class CulumnRule : ISudokuRule<int>
+    {
+        public bool IsValid(int row, int col, ISudokuBoard<int> board, int number)
+        {
+            for (int i = 0; i < board.Size; i++)
+            {
+                if (board[i, col] == number && i != row)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
